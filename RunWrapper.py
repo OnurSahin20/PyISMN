2dd3c95a5b86aa4c58f2cb62a71e9841e04cff8f
OnurSahin20 <ogungorsahin@gmail.com>
2025-04-12 10:13:16 +0300

    some addings

--- RunWrapper.py
+++ RunWrapper.py
@@ -3,6 +3,8 @@ from DataWrapper import DataWrapper
 if __name__ == '__main__':
     path = "ISMN_DataExample"
     ismn_wrapper = DataWrapper(path)
-    scan_stations = ismn_wrapper.get_stations("SCAN")
-    parameters = ismn_wrapper.get_station_data("SCAN", scan_stations[0], daily_hourly="daily")
-    print(ismn_wrapper.get_coordinates("SCAN", scan_stations[0]))
+    networks = ismn_wrapper.get_networks()
+    station_uscrn = ismn_wrapper.get_stations(networks[-1])
+    print(station_uscrn)
+    parameters = ismn_wrapper.get_station_data("USCRN", station_uscrn[0], daily_hourly="daily")
+    print(parameters)
