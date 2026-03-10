e08c5fbc13439baa4db3c8cefba8ede68877c242
onur sahin <onursahin@onurs-MacBook-Air.local>
2026-03-10 11:12:38 +0300

    it works

--- RunWrapper.py
+++ RunWrapper.py
@@ -1,10 +1,11 @@
 from DataWrapper import DataWrapper
+import os 
+
 
 if __name__ == '__main__':
-    path = "ISMN_DataExample"
+    path = os.path.join(os.getcwd(),"ISMN_DataExample")
     ismn_wrapper = DataWrapper(path)
     networks = ismn_wrapper.get_networks()
     station_uscrn = ismn_wrapper.get_stations(networks[-1])
-    print(station_uscrn)
-    parameters = ismn_wrapper.get_station_data("USCRN", station_uscrn[0], daily_hourly="daily")
-    print(parameters)
+    parameters = ismn_wrapper.get_station_data(networks[-1], station_uscrn[0], daily_hourly="daily")
+ 
